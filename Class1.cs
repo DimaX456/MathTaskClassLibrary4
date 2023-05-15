f76d7ba8acfffa0f9457be398ebfb53f27a799c6
DimaX456 <slgwtvshow@mail.ru>
2023-05-15 19:41:31 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathTaskClassLibrary
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус цилиндра: ");
+            double radius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите высоту цилиндра: ");
+            double height = double.Parse(Console.ReadLine());
+
+            double volume = Math.PI * radius * radius * height;
+
+            Console.WriteLine($"Объем цилиндра равен {volume}");
+        }
+    }
+}
